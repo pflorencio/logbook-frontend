adc759cf11980b6ed404c88aba73370236d44e94
pflorencio <patricksflorencio@gmail.com>
2025-12-30 16:32:19 +0000

    cleanup: remove debug smoke tests

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,3 @@
-throw new Error("🚨 LoginPage.tsx is executing");
-
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { fetchUsers, loginUser } from "@/lib/api";
